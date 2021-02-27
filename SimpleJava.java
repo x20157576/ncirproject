81110f23d8c291f225888477a7d34f101940de8f
x20157576 <x20157576@student.ncirl.ie>
2021-02-27 11:22:57 +0000

    My First Commit

---
+++ SimpleJava.java
@@ -0,0 +1,10 @@
+package javaprograms;
+
+public class SimpleJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test");
+	}
+
+}
